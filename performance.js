7bb3af8fd5bb2989c8a5f8c13333ebd57bbab0a4
Keegan Kelly <keegankelly1@gmail.com>
2022-02-07 22:22:47 -0600

    O(n) and O(1)

--- performance.js
+++ performance.js
@@ -1,16 +1,35 @@
 const { performance } = require('perf_hooks');
 
-const pets = ['gibbs'];
+// const pets = new Array(100).fill('gibbs');
 
-function feedPet(array) {
-    let t0 = performance.now();
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === 'gibbs') {
-            console.log(`Fed Gibbs!`);
-        }
-    }
-    let t1 = performance.now();
-    console.log(`Call took ${t1-t0} milliseconds`)
+// function feedPet(array) {
+//     let t0 = performance.now();
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] === 'gibbs') {
+//             console.log(`Fed Gibbs!`);
+//         }
+//     }
+//     let t1 = performance.now();
+//     console.log(`Call took ${t1-t0} milliseconds`)
+// }
+
+// feedPet(pets);
+
+const images = new Array(10).fill('IMAGE')
+
+// O(n)
+function compressAllImages(arr) {
+    arr.forEach(image => {
+        console.log(image);
+    });
 }
 
-feedPet(pets);
+// compressAllImages(images);
+
+
+// O(1)
+function compressFirstImage(arr) {
+    console.log(arr[0]);
+}
+
+compressFirstImage(images);
