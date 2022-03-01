27b4a9588e2041108762a5c891cf5006ea52f743
Keegan Kelly <keegankelly1@gmail.com>
2022-03-01 15:51:31 -0600

    Hash table created.

---
+++ hash.js
@@ -0,0 +1,33 @@
+class HashTable {
+    constructor(size){
+      this.data = new Array(size);
+    }
+  
+    _hash(key) {
+      let hash = 0;
+      for (let i =0; i < key.length; i++){
+          hash = (hash + key.charCodeAt(i) * i) % this.data.length
+      }
+      return hash;
+    }
+
+    set(item, amount) {
+        this.data.unshift([item, amount]);
+    }
+
+    get(item) {
+        for (let index = 0; index < this.data.length; index++) {
+            if (this.data[index].length >= 1) {
+                const data = this.data[index];
+                return data[1];
+            }
+            
+        }
+    }
+  }
+  
+  const myHashTable = new HashTable(50);
+  console.log(myHashTable.set('grapes', 10000))
+  console.log(myHashTable.get('grapes'))
+  console.log(myHashTable.set('apples', 9))
+  console.log(myHashTable.get('apples'))
