66ebfe1eef8dd852a498da03a39c606be314fce8
Keegan Kelly <keegankelly1@gmail.com>
2022-03-10 18:24:18 -0600

    Lookup method added.

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -10,4 +10,67 @@ class BinarySearchTree {
     constructor(){
         this.root = null;
     }
+    lookup(value) {
+        if (!this.root) {
+            return false;
+        }
+
+        let currentNode = this.root;
+        while(currentNode) {
+            if (value < currentNode.value) {
+                currentNode = currentNode.left;
+            }
+            else if (value > currentNode.value) {
+                currentNode = currentNode.right
+            }
+            else if (currentNode.value === value) {
+                return currentNode;
+            }
+        }
+        return false;
+    }
+    insert(value) {
+        const newNode = new Node(value);
+        if (!this.root) {
+            this.root = newNode;
+        }
+        else {
+            let currentNode = this.root;
+            while(true) {
+                if (value < currentNode.value) {
+                    if (!currentNode.left) {
+                        currentNode.left = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.left;
+                }
+                else {
+                    if (value >= currentNode.right) {
+                        currentNode.right = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.right;
+                }
+
+            }
+        }
+    }
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+console.log(tree.lookup(88));
+console.log(JSON.stringify(traverse(tree.root)));
+
+function traverse(node) {
+    const tree = {value: node.value};
+    tree.left = node.left === null ? null : traverse(node.left);
+    tree.right = node.right === null ? null : traverse(node.right);
+    return tree;
 }
