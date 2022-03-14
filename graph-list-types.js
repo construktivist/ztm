8d49dcb9064b636b97fd7edd2494502524d105a7
Keegan Kelly <keegankelly1@gmail.com>
2022-03-14 13:51:51 -0500

    Added graph types

---
+++ graph-list-types.js
@@ -0,0 +1,17 @@
+//Edge List
+const edge = [[0, 2], [2, 3], [2, 1], [1, 3]];
+
+//Adjacent List
+const adjacent_list = [[2], [2, 3], [0, 1, 3], [1, 2]];
+
+//Adjacent Matrix
+const adjacent_matrix = [
+    [0, 0, 1, 0],
+    [0, 0, 1, 1],
+    [1, 1, 0, 1],
+    [0, 1, 1, 0]
+];
+
+console.log(edge);
+console.log(adjacent_list);
+console.log(adjacent_matrix);
