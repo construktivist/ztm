58281d8ec3aa09c0eca4bacccee624be88c978e8
Keegan Kelly <keegankelly1@gmail.com>
2022-03-16 14:44:35 -0500

    reverse string recursive.

--- reverse-string.js
+++ reverse-string.js
@@ -11,9 +11,20 @@ function reverseStringIterative(str) {
 }
 
 console.log(reverseStringIterative('yoyo mastery'));
-//should return: 'yretsam oyoy'
 
 
 function reverseStringRecursive(str) {
 
+    if (str === "") {
+        return "";
+      } 
+      else {
+        return reverseStringRecursive(str.substr(1)) + str.charAt(0);
+      }
 }
+
+console.log(reverseStringRecursive('car'));
+
+
+
+
